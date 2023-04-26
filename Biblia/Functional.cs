527b943bd7bdc02f752ff3863cfc5b234803d4d7
swag <swag@DESKTOP-Q8VEL0I>
2023-04-26 19:45:15 +0300

    Добавьте файлы проекта.

---
+++ Biblia/Functional.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Biblia
+{
+    internal class Functional
+    {
+        int rowCount;
+        int colCount;
+        Excel.Application excelApp = new Excel.Application();//создаем объект приложения
+        Range fileAccess(int numberList)
+        {           
+            Workbook ExcelBook = excelApp.Workbooks.Open(@"C:\Users\swag\source\repos\Biblia\list.xlsx");
+            _Worksheet worksheet = (Worksheet)ExcelBook.Worksheets[numberList];//выбираем лист
+            Range excelRange = worksheet.UsedRange;//найти используемые ячейки в массиве
+            return excelRange;
+        }
+        public void conclusionReaders()//выыод должников
+        {
+            Range excelRange = fileAccess(1);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {                   
+                    if ((excelRange.Cells[i, j] != null) && (excelRange.Cells[i, j].Value2 != null) && (excelRange.Cells[i, 6].Value2 == null))
+                    { 
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");
+                        if (j == (colCount - 1))
+                        {
+                            Console.Write("\r\n");
+                        } 
+                    }
+                    
+                }
+            }          
+            excelApp.Quit();
+            Marshal.FinalReleaseComObject(excelApp);
+        }
+
+        public void bookList() 
+        {
+            Range excelRange = fileAccess(2);
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    if (excelRange.Cells[i, j].Value2 == null)
+                    {
+                        Console.Write("\t");
+                    }
+                    if ((excelRange.Cells[i, j] != null) && (excelRange.Cells[i, j].Value2 != null))
+                    {
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t");                      
+                    }
+                }
+             Console.WriteLine();
+            }
+            excelApp.Quit();
+            Marshal.FinalReleaseComObject(excelApp);
+        }
+    }
+}
---
+++ Biblia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Biblia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Functional fun = new Functional();
+            int answer = 0;
+            do
+            {
+                Console.WriteLine("1-Просмотр списка читателей, не вернувших книгу" +
+                    "2- ");
+                answer = Convert.ToInt32(Console.ReadLine());
+            } while (answer == 1);
+            //fun.conclusionReaders();
+            fun.bookList();
+            Console.ReadLine();
+        }
+    }
+}
